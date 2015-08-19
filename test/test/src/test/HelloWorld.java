be4da24ed2132ff8ff34360ae46510fccd09e8e4
Sophie <sophie.x.yu@gmail.com>
2015-08-18 20:33:26 -0700

    My first java class

---
+++ test/test/src/test/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package test;
+
+/**
+ * @author Sophie
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
